e87f20d0f53a220ed4442536474f9dd26674dca3
Simon Bukin <simonbukin@gmail.com>
2023-12-17 10:43:23 -0800

    Add responsive styling to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,25 @@
 
 <HeroText />
 <h1 class="font-bold text-3xl my-4">projects.</h1>
-<div class=" grid grid-cols-2 gap-4">
+<div class="projects">
 	{#each projects as project}
 		<Project {...project} />
 	{/each}
 </div>
+
+<style>
+	.projects {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-gap: 1rem;
+	}
+	@media (max-width: 640px) {
+		.projects {
+			display: flex;
+			flex-direction: column;
+		}
+		h1 {
+			font-size: 1.5rem;
+		}
+	}
+</style>
