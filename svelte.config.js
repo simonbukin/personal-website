84c2dc215256a50b602694c9c6c16af89bdadc1a
Simon Bukin <simonbukin@gmail.com>
2023-12-17 20:34:15 -0800

    Fix prerender on dynamic routes

--- svelte.config.js
+++ svelte.config.js
@@ -30,7 +30,20 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			crawl: true,
+			enabled: true,
+			onError: 'fail',
+			entries: [
+				'/blog/beep-boop',
+				'/blog/logging-spotify',
+				'/blog/python-tips',
+				'/blog/python-virtual-environments',
+				'/portfolio/gcs-security',
+				'/portfolio/sagemaker-onboarding'
+			]
+		}
 	}
 };
 
