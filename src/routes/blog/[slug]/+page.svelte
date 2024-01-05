a94687d2aca980335188a5cf28715585a8d15814
Simon Bukin <8992420+simonbukin@users.noreply.github.com>
2024-01-04 22:27:58 -0800

    Add reading time to blog posts ⏰

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,31 +1,33 @@
 <script>
-	import BackToTop from '$lib/components/BackToTop.svelte';
-	import ScrollableWrapper from '$lib/components/ScrollableWrapper.svelte';
+	import HoverLink from '$lib/components/HoverLink.svelte';
 	import { formatDateString } from '$lib/utils';
+	import Readotron from '@untemps/svelte-readotron';
 
 	export let data;
+	const { title, description, date } = data.meta;
 </script>
 
 <svelte:head>
 	<title>{data.meta.title}</title>
 	<meta property="og:type" content="article" />
-	<meta property="og:title" content={data.meta.title} />
-	<meta name="description" content={data.meta.description} />
+	<meta property="og:title" content={title} />
+	<meta name="description" content={description} />
 </svelte:head>
 
 <article class="mt-8">
 	<hgroup>
-		<h1 class="text-shadow">{data.meta.title}</h1>
+		<h1 class="text-shadow">{title}</h1>
 		<h2>
-			{formatDateString(data.meta.date, {
+			{formatDateString(date, {
 				monthFormat: 'long',
 				dayFormat: 'suffix',
 				yearFormat: 'full'
 			})}
 		</h2>
+		<Readotron class="border-b-2 border-b-orange-300 text-xl" selector=".prose" />
 	</hgroup>
 	<div class="prose">
-		<svelte:component this={data.content} />
+		<svelte:component this={data.content} class="content" />
 	</div>
 </article>
 
