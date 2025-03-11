6dafbd8258db62100f721dae9a4a3fc71d518554
Simon Bukin <8992420+simonbukin@users.noreply.github.com>
2025-03-10 23:04:49 -0700

    Update about and using

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,78 +10,61 @@
 		Hello! I'm Simon Bukin, a design engineer based out of San Francisco, CA. I
 		live here with my wife and two cats, <a
 			target="_blank"
-			href="https://yugipedia.com/wiki/Blue-Eyes_White_Dragon">Blue</a
+			href="https://yugipedia.com/wiki/Blue-Eyes_White_Dragon"
+			class="text-fuchsia-300">Blue</a
 		>
 		and
-		<a target="_blank" href="https://scryfall.com/card/cmm/5/ulamog-the-ceaseless-hunger">Mog</a>. I
+		<a target="_blank" href="https://scryfall.com/card/cmm/5/ulamog-the-ceaseless-hunger" class="text-fuchsia-300">Mog</a>. I
 		have a BS in Computer Science and specialize primarily in crafting
 		quality user experiences. I'm always
-		<a target="_blank" href="https://www.github.com/simonbukin">working on something new</a>!
+		<a target="_blank" href="https://www.github.com/simonbukin" class="text-fuchsia-300">working on something new</a>!
 	</p>
 	<p>I've had the privilege of working at some excellent places, including the following:</p>
 
 	<ul>
-		<li>Software Development Engineer @ <a href="https://amazon.com">Amazon</a></li>
+		<li>Software Development Engineer @ <a href="https://amazon.com" class="text-fuchsia-300">Amazon</a></li>
 		<li>
 			Frontend Software Development Engineer @ Amazon Web Services, <a
-				href="/portfolio/sagemaker-onboarding">SageMaker Console</a
+				href="/portfolio/sagemaker-onboarding" class="text-fuchsia-300">SageMaker Console</a
 			>
 		</li>
-		<li>Software Engineer @ Google, <a href="https://www.youtube.com/premium">YouTube Premium</a></li>
-		<li>User Experience Engineer @ <a href="/portfolio/gcs-security">Google Cloud Storage</a></li>
-		<li>Founding Frontend Engineer @ <a href="https://archipelago-ai.com">Archipelago AI</a></li>
-		<li>Design Engineer @ <a href="https://withintrinsic.com/">Intrinsic</a></li>
-		<li>Product Engineer @ <a href="https://morf.health">Morf Health</a></li>
+		<li>Software Engineer @ Google, <a href="https://www.youtube.com/premium" class="text-fuchsia-300">YouTube Premium</a></li>
+		<li>User Experience Engineer @ <a href="/portfolio/gcs-security" class="text-fuchsia-300">Google Cloud Storage</a></li>
+		<li>Founding Frontend Engineer @ <a href="https://archipelago-ai.com" class="text-fuchsia-300">Archipelago AI</a></li>
+		<li>Design Engineer @ <a href="https://withintrinsic.com/" class="text-fuchsia-300">Intrinsic</a></li>
+		<li>Product Engineer @ <a href="https://morf.health" class="text-fuchsia-300">Morf Health</a></li>
 	</ul>
 
 	<p>Outside of work, I enjoy a smorgasbord of things, including but not limited to:</p>
 	<ul>
-		<li>Fiddling with different ways of making coffee (French press is nice).</li>
-		<li>Producing music (mostly just copying <a target="_blank" href="https://www.youtube.com/watch?v=DCFvNFafeVA">A.G. Cook</a>).</li>
+		<li>Producing music (mostly just copying <a target="_blank" href="https://www.youtube.com/watch?v=DCFvNFafeVA" class="text-fuchsia-300">A.G. Cook</a>).</li>
 		<li>Collecting vinyl records.</li>
 		<li>Learning Japanese (🇯🇵)</li>
 		<li>
-			Reading <a target="_blank" href="https://hardcover.app/@simon">new books</a> (and rereading the
+			Reading <a target="_blank" href="https://hardcover.app/@simon" class="text-fuchsia-300">new books</a> (and rereading the
 			Three Body Problem series).
 		</li>
 		<li>
-			Listening to <a target="_blank" href="https://www.last.fm/user/sbukin_">any music</a> I can get
+			Listening to <a target="_blank" href="https://www.last.fm/user/sbukin_" class="text-fuchsia-300">any music</a> I can get
 			my ears on.
 		</li>
 	</ul>
 	
-	<details>
-		<summary>...but this list changes all the time.</summary>
+	<details class="mt-4 cursor-pointer group">
+		<summary class="italic mb-4 group-open:mb-4">...but this list changes all the time.</summary>
 		<ul>
 		<li>Climbing ~V2s (but I did climb a V3 once!)</li>
 			<li>
 				Photography and birds. <a
 					target="_blank"
-					href="https://ebird.org/profile/MzYwODk4OQ/US-CA-087">Maybe both at once</a
+					href="https://ebird.org/profile/MzYwODk4OQ/US-CA-087"
+					class="text-fuchsia-300">Maybe both at once</a
 				>.
 			</li>
 			<li>Polishing my Russian (🇷🇺) from "heritage speaker" to "native speaker".</li>
 			<li>Learning German (🇩🇪) at around a B2 level (around A2 - B1 right now).</li>
 			<li>Fiddling with different ways of making coffee (French press is nice).</li>
+			<li>Fiddling with different ways of making coffee (French press is nice).</li>
 		</ul>
 	</details>
 </article>
-
-<style>
-	a {
-		color: #f0abfc;
-	}
-	
-	details {
-		margin-top: 1rem;
-		cursor: pointer;
-	}
-	
-	summary {
-		font-style: italic;
-	}
-	
-	details[open] summary {
-		margin-bottom: 1rem;
-	}
-</style>
--- src/routes/using/+page.svelte
+++ src/routes/using/+page.svelte
@@ -9,20 +9,27 @@
 
 <h2 class="text-xl">Software</h2>
 <ul>
-  <li>Arc</li>
-  <li>Alacritty (though I've used Warp and iTerm2 before)</li>
-  <li>Cursor (though I've wanted to swap to Neovim for ages)</li>
+  <li><a href="https://arc.net">Arc + Arc on Mobile</a></li>
+  <li><a href="https://alacritty.org">Alacritty</a>(though I've used Warp and iTerm2 before)</li>
+  <li><a href="https://obsidian.md">Obsidian</a></li>
 </ul>
 <h2 class="text-xl">Theming</h2>
+<p>I mostly use light theme where I can (I don't know how this happened)</p>
 <ul>
-  <li>Catpuccin Mocha</li>
-  <li>Iosevka Font</li>
+  <li><a href="https://catppuccin.com">Catpuccin Mocha</a></li>
+  <li><a href="https://typeof.net/Iosevka/">Iosevka Font</a></li>
 </ul>
 <h2 class="text-xl">AI Tooling</h2>
 <ul>
-  <li>Cursor w/ Sonnet 3.5</li>
+  <li><a href="https://www.cursor.com">Cursor</a></li>
+  <li><a href="https://www.perplexity.com">Perplexity</a></li>
+  <li><a href="https://repomix.com">Repomix</a></li>
 </ul>
-
+<h2 class="text-xl">Music</h2>
+<ul>
+	<li>Spotify (though I've tried to migrate off of it multiple times. Local music hosting is hard.)</li>
+	<li><a href="https://rateyourmusic.com">Rate Your Music</a></li>
+</ul>
 <style>
 	.text-shadow {
 		text-shadow:
